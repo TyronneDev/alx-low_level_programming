39c74c1ec17f53be9f1d37769901d94d511dba4e
TyronneDev <tyronne.j@outlook.com>
2023-02-17 04:30:05 +0000

    changes made to 4,5,6 and 101

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-/* Fa no fom */
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
 
 int main(void)
 {
