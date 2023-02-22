758930f55d9424746f4707633cb90f2d883674b6
TyronneDev <tyronne.j@outlook.com>
2023-02-22 04:37:37 +0000

    print alphabets in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- *
  * print_alphabet -  print the alphabets in lower case using putchar
  *
 */
 
+/* print alphabet prints alphabets in lower case */
 void print_alphabet(void)
 {
 	int ch;
