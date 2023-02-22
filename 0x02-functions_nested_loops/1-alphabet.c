49bfe2f8d7e461040ce29ceac71d2a10133e8e59
TyronneDev <tyronne.j@outlook.com>
2023-02-22 04:25:16 +0000

    print alphabets in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * print_alphabet - print the alphabets in lower case using putchar
+ * Description: print the alphabets in lower case using putchar
  *
  * Return: Always 0 (Successs)
 */
