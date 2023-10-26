60d28e154f2dfb44b4e68335bbab51208163e8d5
TyronneDev <tyronne.j@outlook.com>
2023-10-26 13:07:36 +0000

    prints a sting in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
 * _print_rev_recursion - function that prints a string in reverse
@@ -8,7 +8,7 @@ i#include "main.h"
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 		return; 
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
