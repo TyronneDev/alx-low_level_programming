13b89d51dfa0177ac74c79677f2db96adfa041c6
TyronneDev <tyronne.j@outlook.com>
2023-10-26 10:01:06 +0000

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,18 @@
 /**
 * _puts_recursion - function that prints a string
 * @s: pointer to string
-* Return: void
+* return: void
 **/
 
 void _puts_recursion(char *s)
 {
-        if (*s == "\0")
-        {
-                _putchar(\n);
-                return;
-        }
-
-        _putchar(*s); _puts_recursion(s+1);
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+	
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
