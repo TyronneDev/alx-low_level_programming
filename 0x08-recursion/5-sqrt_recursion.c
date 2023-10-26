37fe6c403422d505739d2c8ca3ff3a60105bbb0d
TyronneDev <tyronne.j@outlook.com>
2023-10-26 18:10:01 +0000

    returns the natural square of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - returns natural square root of a number
+* @n: int
+* return: natural square root or -1 if there's no square root for n
+**/
+int _sqrt_recursion(int n)
+{
+return (natural_square(n, 1));
+}
+
+/**
+* natural_square - function that help to find natural_square
+* @x: int
+* @y: int
+* Return: square root or -1
+**/
+
+int natural_square(int x, int y)
+{
+	int s = y * y;
+	if (s > x)
+		return (-1);
+	if (s == x)
+		return (y);
+	return (natural_square(x, y + 1));
+}
