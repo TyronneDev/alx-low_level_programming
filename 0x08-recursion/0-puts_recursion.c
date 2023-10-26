608dc1332f0d31f5eaeed0dbb0dc573aa1f20af8
TyronneDev <tyronne.j@outlook.com>
2023-10-26 05:14:45 +0000

    prints a string followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,9 @@ void _puts_recursion(char *s)
         //base case
         if (*s == "\0")
         {
-                _putchar(*s);
+                _putchar(\n);
                 return;
         }
 
-        _putchar(*s) + _puts_recursion(s+1);
+        _putchar(*s)  _puts_recursion(s+1);
 }
