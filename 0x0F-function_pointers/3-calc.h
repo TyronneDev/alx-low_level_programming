4257bc473fc4d0dbcbebb8bb0fb0dcc71aa21465
TyronneDev <tyronne.j@outlook.com>
2023-11-09 05:47:01 +0000

    program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef _3_CALC_H_
 #define _3_CALC_H_
+
 /**
  * struct op - Struct op
  *
@@ -18,4 +19,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* _3_CALC_H */
+#endif /* _3_CALC_H_ */
