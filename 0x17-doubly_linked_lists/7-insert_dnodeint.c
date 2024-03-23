7eb387896801c1cfd35178be50464e810366704b
TyronneDev <tyronne.j@outlook.com>
2024-03-23 05:29:33 +0000

    made corrections

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,37 +10,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node = *h;
-	dlistint_t *new_node;
-	unsigned int i = 0;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	new_node  = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (h == NULL)
-		return (NULL);
+	new = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	while (current_node != NULL)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (i == idx - 1 && current_node->next == NULL)
-			return (add_dnodeint_end(h, n));
-		if (i == idx - 1)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			new_node->next = current_node->next;
-			new_node->prev = current_node;
-			current_node->next = new_node;
-			current_node = new_node;
-			current_node->next->prev = new_node;
-			return (new_node);
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
 		}
-		current_node = current_node->next;
-		i++;
 	}
-	return (NULL);
+
+	return (new);
 }
